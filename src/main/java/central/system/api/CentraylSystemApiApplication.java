9ea75c9d900579dc5a0af376dde70047dfb25a75
jennifer campero couceiro <jenny270294@gmail.com>
2020-07-08 10:13:22 +0200

    fist commit

---
+++ src/main/java/central/system/api/CentraylSystemApiApplication.java
@@ -0,0 +1,13 @@
+package central.system.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentraylSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentraylSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/central/system/api/CentraylSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package central.system.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentraylSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
